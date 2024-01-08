b2941da08effe6e91b8eda12f7d085d46b3ed32e
Abhishek Srivastava <abhi.brewer@gmail.com>
2024-01-08 11:53:32 +0530

    Add test cases - loginWithWrongUsername and loginWithWrongPassword.

--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -40,5 +40,67 @@ public class LoginTest {
 
 		driver.close();
 	}
+	@Test
+	public void loginWithWrongUsername() {
+
+	    WebDriverManager.chromedriver().setup();
+	    WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.get("https://www.saucedemo.com/");
+
+	    try {
+	        Thread.sleep(2000);
+	    } catch (InterruptedException e) {
+
+	    }
+
+	    WebElement usernameTextBox = driver.findElement(By.id("user-name"));
+	    usernameTextBox.sendKeys("incorrect_user");
+	    WebElement passwordTextBox = driver.findElement(By.id("password"));
+	    passwordTextBox.sendKeys("secret_sauce");
+	    WebElement loginButton = driver.findElement(By.id("login-button"));
+	    loginButton.click();
+
+	    WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"));
+	    if (errorMessage.isDisplayed()) {
+	        System.out.println("Login failed with incorrect username. Error message: " + errorMessage.getText());
+	    } else {
+	        System.out.println("Unexpected behavior. Error message not displayed for incorrect username.");
+	    }
+
+	    driver.close();
+	}
+	@Test
+	public void loginWithWrongPassword() {
+
+	    WebDriverManager.chromedriver().setup();
+	    WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.get("https://www.saucedemo.com/");
+
+	    try {
+	        Thread.sleep(2000);
+	    } catch (InterruptedException e) {
+
+	    }
+
+	    WebElement usernameTextBox = driver.findElement(By.id("user-name"));
+	    usernameTextBox.sendKeys("standard_user");
+	    WebElement passwordTextBox = driver.findElement(By.id("password"));
+	    passwordTextBox.sendKeys("incorrect_password");
+	    WebElement loginButton = driver.findElement(By.id("login-button"));
+	    loginButton.click();
+
+	    WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"));
+	    if (errorMessage.isDisplayed()) {
+	        System.out.println("Login failed with incorrect password. Error message: " + errorMessage.getText());
+	    } else {
+	        System.out.println("Unexpected behavior. Error message not displayed for incorrect password.");
+	    }
+
+	    driver.quit();
+	}
+
+
 
 }
